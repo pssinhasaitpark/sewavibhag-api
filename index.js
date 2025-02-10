a01e3b279da80859e98b1de151c90ce82d206b19
parkhya.itpark <parkhya.itpark@gmail.com>
2025-02-10 10:32:56 +0530

    Added morgan and ceteralize error handling middleware

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const routes = require('./app/routes/index');
 const connectDB = require('./app/dbConfig/db-config');
 const path = require('path');
 require('dotenv').config();
-
-// Import the database connection function
+const morgan = require('morgan');
 
 const app = express();
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-// Middleware setup for CORS
 
+// Middleware setup for CORS
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:3001", 'https://sewavibhag-client.pages.dev'],
     methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
 
+// Use Morgan for logging
+app.use(morgan('dev'));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -39,9 +41,15 @@ app.get('/', (req, res) => {
     });
 });
 
-
+// Render the report form
 app.get('/report-form', (req, res) => {
     res.render('reportingForm'); // This will render the reportingForm.ejs template
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
 });
 
 // Start the server and listen on a specified port (default: 5000)
